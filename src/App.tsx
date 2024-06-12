0d302e70989fcc8fb6eee0a4951af6af4ca0d548
dmockles <dmockles@hagerty.com>
2024-06-12 15:14:41 -0700

    new app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React, { useState } from "react";
-import TodoList from "./components/TodoList";
-import NewTodo from "./components/NewTodo";
-import { Todo } from "./todo.model";
+import HelloWorld from "./components/HelloWorld";
 
 // function App() {
 //   return (
@@ -10,23 +8,10 @@ import { Todo } from "./todo.model";
 // }
 
 const App: React.FC = () => {
-  const [todos, setTodos] = useState<Todo[]>([]);
-
-  const todoAddHandler = (text: string) => {
-    setTodos((preTodos) => [
-      ...preTodos,
-      { id: Math.random().toString(), text: text },
-    ]);
-  };
-
-  const todoDeleteHandler = (todoId: string) => {
-    setTodos((preTodos) => preTodos.filter((it) => it.id !== todoId));
-  };
 
   return (
     <div className="App">
-      <NewTodo onAddTodo={todoAddHandler} />
-      <TodoList todos={todos} onDeleteTodo={todoDeleteHandler} />
+      <HelloWorld />
     </div>
   );
 };
---
+++ src/components/HelloWorld.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HelloWorld = () => {
+    return (
+        <div>
+            <h1>Hello Smart Mask react version!</h1>
+        </div>
+    );
+}
+
+export default HelloWorld;
